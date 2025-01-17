578afe613ba42f1cfeb10d35720985b465fda46a
ZED-Magdy <zedmagdy@gmail.com>
2025-01-17 16:56:30 +0200

    feat: Add Hibernate, JPA, and connect to mysql

--- src/main/java/com/learn/spring_boot/HelloController.java
+++ src/main/java/com/learn/spring_boot/HelloController.java
@@ -2,14 +2,20 @@ package com.learn.spring_boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
 @RestController()
 public class HelloController {
 
+  @PersistenceContext
+  private EntityManager entityManager;
 
   @GetMapping("/")
   public String Index(){
-    return "Hello, World";
+    Query query = entityManager.createNativeQuery("SELECT 1");
+    return "Database connection successful! Result: " + query.getSingleResult();
   }
   
 }
