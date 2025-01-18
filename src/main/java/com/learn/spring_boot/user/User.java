7261af91516a32b82c666bc2383d03dae0198334
ZED-Magdy <zedmagdy@gmail.com>
2025-01-18 15:05:43 +0200

    feat: Integrate Lombok for User and UserDto classes to reduce boilerplate code

--- src/main/java/com/learn/spring_boot/user/User.java
+++ src/main/java/com/learn/spring_boot/user/User.java
@@ -1,93 +1,36 @@
 package com.learn.spring_boot.user;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 @Entity
 @Table
 public class User {
     @Id
-    @SequenceGenerator(
-            name = "user_seq",
-            sequenceName = "user_seq",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "user_seq"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private Integer age;
 
-    public User() {
-    }
-
-    public User(long id, String name, String email, LocalDate dob, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
     public User(String name, String email, LocalDate dob, Integer age) {
         this.name = name;
         this.email = email;
         this.dob = dob;
         this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
     }
 }
--- src/main/java/com/learn/spring_boot/user/UserDto.java
+++ src/main/java/com/learn/spring_boot/user/UserDto.java
@@ -2,43 +2,15 @@ package com.learn.spring_boot.user;
 
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
     public Long id;
     public String name;
     public String email;
     public LocalDate dob;
     public int age;
-    
-    public Long getId() {
-      return id;
-    }
-    public void setId(Long id) {
-      this.id = id;
-    }
-    public String getName() {
-      return name;
-    }
-    public void setName(String name) {
-      this.name = name;
-    }
-    public String getEmail() {
-      return email;
-    }
-    public void setEmail(String email) {
-      this.email = email;
-    }
-    public LocalDate getDob() {
-      return dob;
-    }
-    public void setDob(LocalDate dob) {
-      this.dob = dob;
-    }
-    public int getAge() {
-      return age;
-    }
-    public void setAge(int age) {
-      this.age = age;
-    }
-
-    
 }
